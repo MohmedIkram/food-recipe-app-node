7cb73cf150f3bf17d1a667c0aff2d7a64e57ae41
mohmedikram123@gmail.com <mohmedikram123@gmail.com>
2021-09-08 13:01:28 +0530

    update

--- routes/recipe.js
+++ routes/recipe.js
@@ -2,8 +2,6 @@ import express from "express";
 import { Recipes } from "../models/recipe.js";
 const router = express.Router();
 
-// 5x + 6x = x(5 +6)
-
 router
   .route("/")
   .get(async (request, respone) => {
@@ -13,7 +11,7 @@ router
     if (request.query.title) {
       request.query.title = new RegExp("^" + request.query.title, "i");
     }
-    // /idly/
+
     console.log("after", request.query);
 
     const recipe = await Recipes.find(request.query);
@@ -40,49 +38,3 @@ router.get("/:id", async (request, respone) => {
 });
 
 export default router;
-// Default exports
-// import router from "./routes/recipe.js";
-// import recipeRouter from "./routes/recipe.js";
-
-// const obj = {};
-// obj.a = '1'
-// obj.b = (a, b) => a + b;
-// obj.d = 10;
-// export default obj;
-
-// commonjs
-// module.exports = {
-//    recipeRouter: router
-// }
-
-// sqlite
-
-// {
-//   name: "Chinmay",
-//   profile: "http://profile"
-// }
-
-// // React or post man
-// // Patch request "/users/123"
-// {
-//   profile: "http://newprofile"
-// }
-
-// // Database
-// {
-//   name: "Chinmay",
-//   profile: "http://newprofile"
-// }
-
-// // React or post man
-// // Put request "/users/123" - Web standard
-// {
-//   profile: "http://newprofile"
-// }
-
-// // Database all other keys will be deleted
-// {
-//   profile: "http://newprofile"
-// }
-const inDbStoredPassword =
-  "$2b$10$ClN9lkayRBtSTPShLDkKa.OCnxbnNoPQoAXpraQJrKQj2Wvd8tdTu";
